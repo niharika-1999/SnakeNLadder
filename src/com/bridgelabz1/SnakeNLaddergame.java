ffa8008fb1a98f7cba54d8136a3416799d0cc331
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-21 13:42:41 +0530

    Added start position

---
+++ src/com/bridgelabz1/SnakeNLaddergame.java
@@ -0,0 +1,21 @@
+package com.bridgelabz1;
+import java.util.Random;
+class SnakeNLadder{
+
+//SnakeNLadder game with single player at start position 0
+
+public class SnakeNLaddergame {
+	 public static void main(String[] args) {
+		 
+		 SnakeNLadder s = new SnakeNLadder();
+		 s.Startposition = 0;
+		 
+	 }
+	 
+	
+		
+		
+}		 		
+
+}
+
