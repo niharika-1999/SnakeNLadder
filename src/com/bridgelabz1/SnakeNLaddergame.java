27c62305c428e8c6cfbdd70ed8d95c56e50a88ea
niharika-1999 <niharika.rao@ymedialabs.com>
2021-08-21 14:31:00 +0530

    Added:Checking for Options

--- src/com/bridgelabz1/SnakeNLaddergame.java
+++ src/com/bridgelabz1/SnakeNLaddergame.java
@@ -9,6 +9,7 @@ public class SnakeNLaddergame {
 		 
 		 SnakeNLadder s = new SnakeNLadder();
 		 s.Startposition = 0;
+		 System.out.println("Starting position = 0");
 		 
 	 }
 //Roll of dice using RANDOM
@@ -19,6 +20,29 @@ public class SnakeNLaddergame {
 		 Random r = new Random();
 		 n=r.nextInt(7);
 		 return (n==0?1:n);
+		 System.out.println("Press r to roll dice");
+	 }
+//Player Checks for option
+	 
+	 public int PlayerValue(int player , int dicevalue)
+	 {
+		 player=player+dicevalue;
+		 if(null!=Noplay.get(player))
+		 {
+			 System.out.println("Stays in same position");
+			 player = Noplay.get(player);
+		 }
+		 if(null!=snake.get(player))
+		 {
+			 System.out.println("Moves behind");
+			 player = snake.get(player);
+		 }
+		 if(null!=Ladder.get(player))
+		 {
+			 System.out.println("Moves ahead");
+			 player = Ladder.get(player);
+		 }
+				 
 	 }
 	 
 	 
